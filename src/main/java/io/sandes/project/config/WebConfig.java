ae4ab6bb19a867407a79f3f078854e4355641c78
Mateus Sandes <mateussandes@hotmail.com.br>
2025-12-05 16:39:32 -0300

    Re implementado a configuração de cors da aplicação

--- src/main/java/io/sandes/project/config/WebConfig.java
+++ src/main/java/io/sandes/project/config/WebConfig.java
@@ -1,25 +1,25 @@
 package io.sandes.project.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns}")
+    private String corsOrigin = "";
 
     @Bean
-    public WebMvcConfigurer corsConfigurer () {
-        return new WebMvcConfigurer() {
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOrigin.split(",");
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("**")
-                        .allowCredentials(true);
-            }
-        };
+        registry
+                .addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
